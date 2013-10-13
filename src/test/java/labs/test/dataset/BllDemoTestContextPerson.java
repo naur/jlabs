51190ee2bdb42b0d2e15a4d22c90ceae2d301414
naur <naur@live.cn>
2013-10-13 22:29:15 +0800

    home-bllunittest

---
+++ src/test/java/labs/test/dataset/BllDemoTestContextPerson.java
@@ -0,0 +1,20 @@
+package labs.test.dataset;
+
+import labs.entities.Person;
+import labs.entities.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Administrator on 10/13/13.
+ */
+@Configuration
+public class BllDemoTestContextPerson {
+    @Bean
+    public Person person() {
+        Person person = new Person();
+        person.setId(2900);
+        person.setName("BllDemoTestContext Configuration Bean: person");
+        return person;
+    }
+}
