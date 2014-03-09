0dde1c679491beee4e896d477004fcb0b9796684
naur <naur@live.cn>
2014-03-08 17:35:07 -0800

    home

--- src/main/java/labs/research/concurrency/stampedlock/StampedLockSpaceship.java
+++ src/main/java/labs/research/concurrency/stampedlock/StampedLockSpaceship.java
@@ -1,9 +1,10 @@
 package labs.research.concurrency.stampedlock;
 
-import jsr166e.StampedLock;
+//import jsr166e.StampedLock;
 
 public class StampedLockSpaceship implements Spaceship {
-    private final StampedLock lock = new StampedLock();
+
+    //private final StampedLock lock = new StampedLock();
 
     private int x;
     private int y;
@@ -11,35 +12,35 @@ public class StampedLockSpaceship implements Spaceship {
     @Override
     public int readPosition(final int[] coordinates) {
         int tries = 1;
-        long stamp = lock.tryOptimisticRead();
-
-        coordinates[0] = x;
-        coordinates[1] = y;
-
-        if (!lock.validate(stamp)) {
-            ++tries;
-
-            stamp = lock.readLock();
-            try {
-                coordinates[0] = x;
-                coordinates[1] = y;
-            } finally {
-                lock.unlockRead(stamp);
-            }
-        }
+//        long stamp = lock.tryOptimisticRead();
+//
+//        coordinates[0] = x;
+//        coordinates[1] = y;
+//
+//        if (!lock.validate(stamp)) {
+//            ++tries;
+//
+//            stamp = lock.readLock();
+//            try {
+//                coordinates[0] = x;
+//                coordinates[1] = y;
+//            } finally {
+//                lock.unlockRead(stamp);
+//            }
+//        }
 
         return tries;
     }
 
     @Override
     public int move(final int xDelta, final int yDelta) {
-        final long stamp = lock.writeLock();
-        try {
-            x += xDelta;
-            y += yDelta;
-        } finally {
-            lock.unlockWrite(stamp);
-        }
+//        final long stamp = lock.writeLock();
+//        try {
+//            x += xDelta;
+//            y += yDelta;
+//        } finally {
+//            lock.unlockWrite(stamp);
+//        }
 
         return 1;
     }
